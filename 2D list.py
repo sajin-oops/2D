1ff038157af5edc640bffbab9a845966b1d45905
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-08-01 19:51:48 +0530

    Add files via upload

---
+++ 2D list.py
@@ -0,0 +1,9 @@
+tamilnadu = ["chennai","coimbatore","trichy"]
+kerala = ["alapula","kochi","thiruvanduram"]
+karnataka = ["mysore","bangalore"]
+india = [tamilnadu,kerala,karnataka]
+print(india)
+#output
+'''
+[['chennai', 'coimbatore', 'trichy'], ['alapula', 'kochi', 'thiruvanduram'], ['mysore', 'bangalore']]
+'''
